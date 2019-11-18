3aa049ccedd3a0f653c8f2681e8ceaeec516a2d0
alekseivoroshlov <nekobeardetected@gmail.com>
2019-11-18 13:24:19 +0300

    6 урок снова

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -317,7 +317,6 @@ fun fromRoman(roman: String): Int {
     if (!indicator) {
         return -1
     }
-    val plusNumber = mapOf('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000)
     for (item in roman) {
         when (item) {
             'I' -> numbersArr.add(1)
